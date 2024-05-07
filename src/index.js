95957d14e5368beb14ac662e259d962d34249ff2
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2024-05-07 12:32:36 +0300

    v5.2.0

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const {
     SolidFill,
     emptyLine,
     lightningChart,
-    synchronizeAxisIntervals,
     UIOrigins,
     UIElementBuilders,
     UILayoutBuilders,
@@ -31,94 +30,80 @@ fetch(new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pat
                 shortName: 'ECG/EKG',
                 name: 'Electrocardiogram',
                 dataSet: ecg,
-                yStart: -1955,
-                yEnd: 1195,
             },
             {
                 shortName: 'NIBP',
                 name: 'Blood pressure',
                 dataSet: bloodPressure,
-                yStart: 0.475,
-                yEnd: 0.795,
             },
             {
                 shortName: 'BFV',
                 name: 'Blood flow volume',
                 dataSet: bloodVolume,
-                yStart: 0.155,
-                yEnd: 0.445,
             },
             {
                 shortName: 'Sp02',
                 name: 'Blood oxygen saturation',
                 dataSet: bloodOxygenation,
-                yStart: 0.015,
-                yEnd: 0.155,
             },
         ]
-        // NOTE: Using `Dashboard` is no longer recommended for new applications. Find latest recommendations here: https://lightningchart.com/js-charts/docs/basic-topics/grouping-charts/
-        const dashboard = lightningChart({
+        const lc = lightningChart({
             resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
         })
-            .Dashboard({
-                numberOfRows: channels.length,
-                numberOfColumns: 1,
+        const chart = lc
+            .ChartXY({
                 theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
             })
-            .setRowHeight(0, 0.4)
-            .setRowHeight(1, 0.3)
-            .setRowHeight(2, 0.2)
-            .setRowHeight(3, 0.2)
+            .setTitle('Medical Dashboard')
+            .setMouseInteractions(false)
+            .setAutoCursorMode(AutoCursorModes.disabled)
+            .setPadding({ right: 140 })
+        chart.getDefaultAxisY().dispose()
+        const axisECG = chart.addAxisY({ iStack: 3 }).setLength({ relative: 0.4 })
+        const axisNIBP = chart.addAxisY({ iStack: 2 }).setLength({ relative: 0.3 })
+        const axisBFV = chart.addAxisY({ iStack: 1 }).setLength({ relative: 0.2 })
+        const axisSp = chart.addAxisY({ iStack: 0 }).setLength({ relative: 0.2 })
+        const yAxisList = [axisECG, axisNIBP, axisBFV, axisSp]
+        const theme = chart.getTheme()
+        const axisX = chart
+            .getDefaultAxisX()
+            .setTickStrategy(AxisTickStrategies.Time)
+            .setDefaultInterval((state) => ({
+                end: state.dataMax,
+                start: (state.dataMax ?? 0) - TIME_DOMAIN,
+                stopAxisAfter: false,
+            }))
+            .setScrollStrategy(AxisScrollStrategies.progressive)
 
-        const theme = dashboard.getTheme()
-
-        const chartList = channels.map((channel, i) => {
-            const chart = dashboard
-                .createChartXY({ rowIndex: i, columnIndex: 0 })
-                .setPadding({ bottom: 4, top: 4, right: 200, left: 10 })
+        const channelsComponents = channels.map((channel, i) => {
+            const axisY = yAxisList[i]
                 .setMouseInteractions(false)
-                .setAutoCursorMode(AutoCursorModes.disabled)
-            const axisX = chart.getDefaultAxisX().setMouseInteractions(false)
-            const axisY = chart
-                .getDefaultAxisY()
-                .setMouseInteractions(false)
-                .setInterval({ start: channel.yStart, end: channel.yEnd })
                 .setTickStrategy(AxisTickStrategies.Empty)
                 .setStrokeStyle(emptyLine)
-            if (i > 0) {
-                chart.setTitleFillStyle(emptyFill)
-            } else {
-                chart.setTitle('Medical Dashboard')
+                .setAnimationScroll(false)
+            const series = chart
+                .addPointLineAreaSeries({
+                    dataPattern: 'ProgressiveX',
+                    automaticColorIndex: Math.max(i - 1, 0),
+                    yAxis: axisY,
+                })
+                .setAreaFillStyle(emptyFill)
+                .setName(channel.name)
+                .setMaxSampleCount(50_000)
+            if (channel.name === 'Electrocardiogram') {
+                series.setStrokeStyle((stroke) =>
+                    stroke.setFillStyle(
+                        new SolidFill({
+                            color: theme.examples.badGoodColorPalette[theme.examples.badGoodColorPalette.length - 1],
+                        }),
+                    ),
+                )
             }
-            if (i < channels.length - 1) {
-                axisX
-                    .setTickStrategy(AxisTickStrategies.Time, (ticks) =>
-                        ticks
-                            .setMajorTickStyle((majorTicks) =>
-                                majorTicks.setLabelFillStyle(emptyFill).setTickStyle(emptyLine).setTickLength(0).setTickPadding(0),
-                            )
-                            .setMinorTickStyle((minorTicks) =>
-                                minorTicks.setLabelFillStyle(emptyFill).setTickStyle(emptyLine).setTickLength(0).setTickPadding(0),
-                            ),
-                    )
-                    .setStrokeStyle(emptyLine)
-                    .setScrollStrategy(undefined)
-            } else {
-                axisX
-                    .setTickStrategy(AxisTickStrategies.Time)
-                    .setDefaultInterval((state) => ({
-                        end: state.dataMax,
-                        start: (state.dataMax ?? 0) - TIME_DOMAIN,
-                        stopAxisAfter: false,
-                    }))
-                    .setScrollStrategy(AxisScrollStrategies.progressive)
-            }
-            return chart
+            return { axisY, series }
         })
 
-        const uiList = chartList.map((chart, i) => {
-            const axisX = chart.getDefaultAxisX()
-            const axisY = chart.getDefaultAxisY()
+        const uiList = channelsComponents.map((components, i) => {
+            const axisY = components.axisY
             const channel = channels[i]
             const ui = chart
                 .addUIElement(UILayoutBuilders.Column, chart.coordsRelative)
@@ -149,7 +134,7 @@ fetch(new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pat
                     .setPosition(
                         chart.translateCoordinate(
                             { x: axisX.getInterval().end, y: axisY.getInterval().end },
-                            chart.coordsAxis,
+                            { x: axisX, y: axisY },
                             chart.coordsRelative,
                         ),
                     )
@@ -157,6 +142,7 @@ fetch(new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pat
                     .setMargin({ left: 10 })
             }
             chart.onResize(positionUI)
+            requestAnimationFrame(positionUI)
 
             return {
                 labelSampleRate,
@@ -164,40 +150,14 @@ fetch(new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pat
             }
         })
 
-        synchronizeAxisIntervals(...chartList.map((chart) => chart.getDefaultAxisX()))
-
-        const seriesList = chartList.map((chart, i) => {
-            const channel = channels[i]
-            const series = chart
-                .addLineSeries({
-                    dataPattern: {
-                        pattern: 'ProgressiveX',
-                    },
-                    automaticColorIndex: Math.max(i - 1, 0),
-                })
-                .setName(channel.name)
-                .setDataCleaning({ minDataPointCount: 1000 })
-
-            if (channel.name === 'Electrocardiogram') {
-                series.setStrokeStyle((stroke) =>
-                    stroke.setFillStyle(
-                        new SolidFill({
-                            color: theme.examples.badGoodColorPalette[theme.examples.badGoodColorPalette.length - 1],
-                        }),
-                    ),
-                )
-            }
-            return series
-        })
-
         let tSamplePos = window.performance.now()
         let iSampleX = 0
         const addData = () => {
             const tNow = window.performance.now()
-            const seriesNewPoints = seriesList.map((_) => [])
+            const seriesNewPoints = channelsComponents.map((_) => [])
             while (tNow > tSamplePos) {
                 const x = tSamplePos
-                for (let i = 0; i < seriesList.length; i += 1) {
+                for (let i = 0; i < channelsComponents.length; i += 1) {
                     const channel = channels[i]
                     const dataSet = channel.dataSet
                     const sample = dataSet[iSampleX % dataSet.length]
@@ -210,7 +170,7 @@ fetch(new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pat
                 tSamplePos += 1000 / SAMPLE_RATE
                 iSampleX += 1
             }
-            seriesList.forEach((series, i) => series.add(seriesNewPoints[i]))
+            channelsComponents.forEach((comp, i) => comp.series.add(seriesNewPoints[i]))
             channelIncomingDataPointsCount += seriesNewPoints[0].length
             requestAnimationFrame(addData)
         }
